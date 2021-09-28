b1813d50a4d069c7b22ba592bb5121a75c51261b
Vasin Hiran <61160230@go.buu.ac.th>
2021-09-28 20:29:12 +0700

    Write a test suite

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,9 +1,8 @@
 const request = require('supertest')
-const app = require('../index')
+const app = require('../app')
 
 describe('GET /', () => {
     it('responds with "Hello world!"', (done) => {
-        request(app).get('/').expect('Hello world!', done)
+        request(app).get('/').expect({"message":"Hello world!"}, done)
     })
 })
-
